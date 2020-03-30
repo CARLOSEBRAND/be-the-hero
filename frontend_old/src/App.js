477e15d27f1852eb62072bc2bfc91ea2f233ddf5
carlosebrand <carlosebrand@gmail.com>
2020-03-30 00:32:06 -0300

    front-end app

--- frontend_old/src/App.js
+++ frontend_old/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-import './global.css'
-
-import Routes from './routes'
-
 function App() {
   return (
-    <Routes />    
-    
+    <h1>Hello Word</h1>
   );
+   
 }
 
 export default App;
